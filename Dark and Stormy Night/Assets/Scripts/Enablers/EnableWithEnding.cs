ce424a408729a2efbed1f8bf3cfb4125be2e69fd
Nathan Crowe <n.crowe.gamedesign@gmail.com>
2019-08-08 16:45:40 +1000

    Materials, Thunder and Endings Oh My
    Signed By An Ass

--- Dark and Stormy Night/Assets/Scripts/Enablers/EnableWithEnding.cs
+++ Dark and Stormy Night/Assets/Scripts/Enablers/EnableWithEnding.cs
@@ -6,8 +6,10 @@ public class EnableWithEnding : MonoBehaviour {
 
     public List<GameObject> enableObjects = new List<GameObject>();
     public List<GameObject> disableObjects = new List<GameObject>();
-
+    [Space (10)]
     public int endingCompleted;
+    [Space(10)]
+    public List<GameObject> enableIfNotObjects = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +20,10 @@ public class EnableWithEnding : MonoBehaviour {
             for (int i = 0; i < disableObjects.Count; i++)
                 disableObjects[i].SetActive(false);
         }
-	}
+        else
+            for (int i = 0; i < enableIfNotObjects.Count; i++)
+                enableIfNotObjects[i].SetActive(true);
+    }
 	
 	// Update is called once per frame
 	void Update () {
