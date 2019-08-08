786beda8811d61353de2f09cb2b44aeb222c3c72
Nathan Crowe <n.crowe.gamedesign@gmail.com>
2019-08-08 15:08:50 +1000

    The Sound Of Curtains
    Signed By An Ass

---
+++ Dark and Stormy Night/Assets/Scripts/CycleThroughSounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class CycleThroughSounds : MonoBehaviour {
+
+    public List<AudioClip> clips = new List<AudioClip>();
+    public bool shuffle;
+    private int lastPlayed = -1;
+
+    private AudioSource AS;
+
+	// Use this for initialization
+	void Start () {
+        AS = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (AS.isPlaying == false)
+        {
+            int play = 0;
+            if (shuffle)
+                play = Random.Range(0, clips.Count);
+            else
+                play = lastPlayed + 1;
+            if (play >= clips.Count)
+                play = 0;
+            AS.clip = clips[play];
+            AS.Play();
+            lastPlayed = play;
+        }
+
+	}
+}
