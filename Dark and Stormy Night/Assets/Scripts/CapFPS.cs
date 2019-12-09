d2973572796e69f0505f10f8d62a1341e9ae9804
SlenderFox <SlenderFox@outlook.com>
2019-12-09 15:39:41 +1100

    Fps is now capped to 60 by default when the game starts

---
+++ Dark and Stormy Night/Assets/Scripts/CapFPS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapFPS : MonoBehaviour
+{
+    int m_iFrameCap = 60;
+
+	private void Awake()
+    {
+        Application.targetFrameRate = m_iFrameCap;
+        //Debug.Log("Capped");
+    }
+}
